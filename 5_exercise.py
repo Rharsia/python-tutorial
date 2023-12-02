5beb4fd8c59b935b6b8f3e6e873a0193c9149451
Yicheng Zhang <Jan.Zhang@seznam.cz>
2023-12-02 18:41:29 +0100

    Create 5_exercise.py
    Vyresil jsem ukol 5

---
+++ 5_exercise.py
@@ -0,0 +1,46 @@
+# Exercise 5: Check if the first and last number of a list is the same
+
+# Write a function to return True if the first and last number of a given list is same. If numbers are different then return False.
+
+# 3 types of arrays
+
+# tuple - immutable, not to be changed after creation
+ovoce = ("Apple", "Banana")
+ovoce[1] = "Pear"
+ovoce
+
+ovoce = ("Apple", "Pear")
+ovoce
+
+# list
+ovoce = ["Apple", "Banana"]
+ovoce[1] = "Pear"
+ovoce
+
+# dictionary
+znamky_studentu = {"Wilson": 1, "Winona": 3, "Wigfrid": 2}
+znamky_studentu["Winona"]
+znamky_studentu["Wigfrid"] = 1
+znamky_studentu
+
+# task
+lst_1 = [10, 20, 30, 40, 10]
+lst_2 = [13, 47, 26, 345, 12, 324, 234, 23]
+
+lst_1[0]
+lst_1[-1]
+
+lst_1[0] == lst_1[-1]
+
+def Check_first_last(lst):
+    if lst[0] == lst[-1]:
+        answer = True
+    else:
+        answer = False
+
+    print(f"Your list: {lst}")
+    print(f"Result is: {answer}")
+
+Check_first_last([1,2,3,4,5,1])
+Check_first_last(lst_1)
+Check_first_last(lst_2)
