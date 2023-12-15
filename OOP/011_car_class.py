deae1bc8687ae27ea04609bf1f347a737f16721f
Yicheng Zhang <Jan.Zhang@seznam.cz>
2023-12-15 10:53:39 +0100

    Update 011_car_class.py

--- OOP/011_car_class.py
+++ OOP/011_car_class.py
@@ -1,1 +1,17 @@
 # exercise - create a car class
+
+class Car:
+    def __init__(self, brand, type, fuel_type, colour):
+        self.brand = brand
+        self.type = type
+        self.fuel_type = fuel_type
+        self.colour = colour
+
+    def __str__(self):
+        return f"A {self.colour} {self.brand} {self.type} that runs on {self.fuel_type}"
+    
+jaguar = Car("Jaguar", "X-Type", "Diesel", "Black")
+skoda = Car("Škoda", "Octavia", "Benzin", "Black")
+
+print(jaguar)
+print(skoda)
