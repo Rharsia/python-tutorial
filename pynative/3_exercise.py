e9a754f1f46f4e51a5083b6732bfbbb15f993029
Yicheng Zhang <Jan.Zhang@seznam.cz>
2023-12-23 18:07:53 +0100

    Update 3_exercise.py

--- pynative/3_exercise.py
+++ pynative/3_exercise.py
@@ -10,3 +10,17 @@ for i in range(len(str)):
 # jine reseni
 for i in range(0, len(str), 2):
     print(str[i])
+
+# slicing
+print(str)
+
+str[:]
+
+str[2:6]
+
+str[:5]
+
+str[3:]
+
+for pismeno in str[0::2]:
+    print(pismeno)
